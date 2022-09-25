5d3742f760934178ea0d4b2d396c3c20a47cea98
Jericho Gragasin <gragasinjericho26@gmail.com>
2022-09-25 23:40:24 +0800

    Created Wordpair App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: RandomWords(),
+    );
+  }
+}
